f8658508f0ea4b53376a803c77d3ba0b4b882828
hiepsys <bandaychuyen1@gmail.com>
2020-02-07 11:42:58 +0700

    new brand

--- bai2.py
+++ bai2.py
@@ -1,9 +1,8 @@
 #Viết một chương trình có thể tính giai thừa của một số cho trước. Kết quả được in thành chuỗi trên một dòng, phân tách bởi dấu phẩy. Ví dụ, số cho trước là 8 thì kết quả đầu ra phải là 40320.
-a=int(input("nhập số giai thừa:"))
+a=int(input("nhập số cần tình giai thừa:"))
 def giaithua(a):
 	if a == 0 :
 		return 1
 	return a* giaithua(a -1) 
 
 print (giaithua(a))
-print (" giai thua của ",a, "là " giaithua(a))
